7a3a30b9f0aba02b7b44487c0df9499187a978e4
JohnAbraham <55256167+Jrabps@users.noreply.github.com>
2023-03-17 08:25:18 -0400

    day 1 muskegon pre field

--- src/main/java/frc/robot/Auton/alignAndDriveApril.java
+++ src/main/java/frc/robot/Auton/alignAndDriveApril.java
@@ -13,8 +13,6 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.*;
 
-
-
 public class alignAndDriveApril extends CommandBase {
 
     boolean isFinished;
@@ -43,6 +41,9 @@ public class alignAndDriveApril extends CommandBase {
         timer = new Timer();
         visionTimer = new Timer();
         this.xCoord = xCoord;
+        if (!drive.isBlueAlliance()) {
+            xCoord *= -1;
+        }
         this.baseSpeed = baseSpeed;
         this.slowSpeed = slowSpeed;
         this.tolerance = tolerance;
@@ -77,80 +78,89 @@ public class alignAndDriveApril extends CommandBase {
         SmartDashboard.putNumber("angle error", Math.toDegrees(angleError));
         SmartDashboard.putNumber("vision timer", visionTimer.get());
         SmartDashboard.putNumber("dx a", dx);
+
         double currXRobot = drive.getRobotPos().getX();
         angleError = Math.abs(drive.getRobotPos().getRotation().getRadians() - 0);
+
         double tarPointX = xCoord;
         dx = tarPointX - currXRobot;
         double mag = Math.sqrt(Math.pow(dx, 2));
-        double udx = dx/mag;
+        double udx = dx / mag;
 
-        if(mag < slowDownDistance){
-            double slope = (baseSpeed - slowSpeed)/slowDownDistance;
+        // Calculate robot speed
+        if (mag < slowDownDistance) {
+            double slope = (baseSpeed - slowSpeed) / slowDownDistance;
             adjSpeed = slowSpeed + mag * slope;
-        }else{
+        } else {
             adjSpeed = baseSpeed;
         }
 
+        //
         double velX = adjSpeed * udx;
         drive.velY = velX;
-        double sigError = 11-lime.getHorOffset();
-        if(Math.abs(sigError) < 2){
+        double sigError = 11 - lime.getHorOffset();
+
+        if (Math.abs(sigError) < 2) {
             visionTimer.start();
-        }else{
+        } else {
             visionTimer.reset();
         }
-        if(drive.getRobotPos().getRotation().getDegrees() < 10){
-            if(timer.get() > 0.2){
-                error = Math.abs(11-lime.getHorOffset());
+
+        if (drive.getRobotPos().getRotation().getDegrees() < 10) {
+            if (timer.get() > 0.2) {
+                error = Math.abs(11 - lime.getHorOffset());
                 double dir = 1;
-                if(sigError > 0) dir = -1;
-                drive.velX = dir * error/16;
+                if (sigError > 0)
+                    dir = -1;
+                drive.velX = dir * error / 16;
                 drive.omega = 0;
             }
-        }else{
-        
-        double currTheta = Math.toRadians(drive.getFieldAngle());
-        SmartDashboard.putNumber("curr Theta", currTheta);
+        } else {
 
-        double currPosTheta =Math.toRadians(0);
-        double currPosPosTheta = Math.toRadians(-360);
-        double currPosNegTheta = Math.toRadians(360);
+            double currTheta = Math.toRadians(drive.navX.getAngle());
+            SmartDashboard.putNumber("curr Theta", currTheta);
 
-        double dThetaRegErr = currPosTheta - currTheta;
-        double dThetaNegErr = currPosNegTheta - currTheta;
-        double dThetaPosErr = currPosPosTheta - currTheta;
-        double tarTheta = 0;
-        if(Math.abs(dThetaRegErr) > Math.abs(dThetaPosErr) && Math.abs(dThetaNegErr) > Math.abs(dThetaPosErr)){
-            tarTheta = currPosPosTheta;
-        }else if(Math.abs(dThetaRegErr) > Math.abs(dThetaNegErr) && Math.abs(dThetaPosErr) > Math.abs(dThetaNegErr)){
-            tarTheta = currPosNegTheta;
-        }else{
-            tarTheta = currPosTheta;
-        }
-        
+            double currPosTheta = Math.toRadians(0);
+            double currPosPosTheta = Math.toRadians(-360);
+            double currPosNegTheta = Math.toRadians(360);
 
-        double dTheta = tarTheta - currTheta;
-        angleError = dTheta;
-        double omega = 0;
+            double dThetaRegErr = currPosTheta - currTheta;
+            double dThetaNegErr = currPosNegTheta - currTheta;
+            double dThetaPosErr = currPosPosTheta - currTheta;
+            double tarTheta = 0;
+            
+            if (Math.abs(dThetaRegErr) > Math.abs(dThetaPosErr) && Math.abs(dThetaNegErr) > Math.abs(dThetaPosErr)) {
+                tarTheta = currPosPosTheta;
+            } else if (Math.abs(dThetaRegErr) > Math.abs(dThetaNegErr)
+                    && Math.abs(dThetaPosErr) > Math.abs(dThetaNegErr)) {
+                tarTheta = currPosNegTheta;
+            } else {
+                tarTheta = currPosTheta;
+            }
 
-        //NOTE tarTheta IS IN DEGREES BUT OUTPUT WILL BE IN RAD/SEC
-        //This part sets the omega based on how far we are from the desired theta
-        if(Math.abs(dTheta) < Constants.thresholdT1){
-            omega = Constants.tVel1;
-        }else if(Math.abs(dTheta) <Constants.thresholdT2){
-            omega = Constants.tVel2;
-        }else if(Math.abs(dTheta) < Constants.thresholdT3){
-            omega = Constants.tVel3;
-        }else{
-            omega = Constants.tVelOutside;
-        }
+            double dTheta = tarTheta - currTheta;
+            angleError = dTheta;
+            double omega = 0;
 
-        //THE SIGNS MIGHT NEED TO BE FLIPPED
-        //THIS PART OF THE CODE ADJUSTS THE DIRECTION OF THE OMEGA SO THAT IT GOES THE CORRECT DIRECTION BASED ON WHETHER OUR ERROR IS POSITIVE OR NEGATIVE
-         if(dTheta < 0){
-             omega = -1 * omega;
-          }
-          drive.omega = omega;
+            // NOTE tarTheta IS IN DEGREES BUT OUTPUT WILL BE IN RAD/SEC
+            // This part sets the omega based on how far we are from the desired theta
+            if (Math.abs(dTheta) < Constants.thresholdT1) {
+                omega = Constants.tVel1;
+            } else if (Math.abs(dTheta) < Constants.thresholdT2) {
+                omega = Constants.tVel2;
+            } else if (Math.abs(dTheta) < Constants.thresholdT3) {
+                omega = Constants.tVel3;
+            } else {
+                omega = Constants.tVelOutside;
+            }
+
+            // THE SIGNS MIGHT NEED TO BE FLIPPED
+            // THIS PART OF THE CODE ADJUSTS THE DIRECTION OF THE OMEGA SO THAT IT GOES THE
+            // CORRECT DIRECTION BASED ON WHETHER OUR ERROR IS POSITIVE OR NEGATIVE
+            if (dTheta < 0) {
+                omega = -1 * omega;
+            }
+            drive.omega = omega;
         }
     }
 
